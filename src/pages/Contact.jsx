ca020c131df6fdf89d59be757c1e31450a45b001
Jupiter <logan.aresche@anima.edu.uy>
2024-06-21 22:04:53 -0300

    Navbar and contact jsx

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+import NavbarComponent from "../components/Navbar";
+
+export default function Contact() {
+    return (
+        <div className="contariner">
+    <NavbarComponent />
+    </div>
+    )
+}
