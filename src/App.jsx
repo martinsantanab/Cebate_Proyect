dda547bf63422e0d8b3c747726691becaa05fada
Namzug <guzman.deleonalonso@gmail.com>
2024-07-07 21:27:38 -0300

    Revert "Merge branch 'main' of https://github.com/martinsantanab/Cebate_Proyect"
    This reverts commit 8dbbf7d9fbaa30675f8793c3d8cfcd47e50be7fe, reversing
    changes made to 577e07f335f5c622e9457056e9a1f08f2de0b05f.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import AboutUs from './pages/AboutUs';
 import Contact from './pages/Contact';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Products from './pages/Products';
 import './App.css';
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/AboutUs" element={<AboutUs />} />
         <Route path="/Contact" element={<Contact />} />
-        <Route path="/Productos" element={<Products />} />
       </Routes>
       <Footer />
     </Router>
